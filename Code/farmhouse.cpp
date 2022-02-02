c16091c59e20f2cf801a8d3e8aff8881adc32103
KyrillW <75068697+KyrillW@users.noreply.github.com>
2022-02-02 13:56:10 +0100

    Update farmhouse.cpp

--- Code/farmhouse.cpp
+++ Code/farmhouse.cpp
@@ -7,13 +7,7 @@ farmhouse::farmhouse(sf::Vector2f position):
         right_collider(position.x+208, position.y+84, 20, 165),
         lower_left_collider(position.x+36, position.y+249, 36, 7),
         lower_right_collider(position.x+192, position.y+249, 36, 7),
-        collider_change(position.x+105, position.y+100, 50, 20),
-        upper_rectangle(sf::Vector2f(position.x+41, position.y+64), sf::Vector2f(182, 10), sf::Color::Blue),
-        left_rectangle(sf::Vector2f(position.x+36, position.y+64), sf::Vector2f(5, 185), sf::Color::Yellow),
-        right_rectangle(sf::Vector2f(position.x+223, position.y+64), sf::Vector2f(5, 185), sf::Color::Yellow),
-        lower_left_rectangle(sf::Vector2f(position.x+36, position.y+249), sf::Vector2f(36, 7), sf::Color::Blue),
-        lower_right_rectangle(sf::Vector2f(position.x+192, position.y+249), sf::Vector2f(36, 7), sf::Color::Blue),
-        rectangle_change(sf::Vector2f(position.x+105, position.y+100), sf::Vector2f(50, 20), sf::Color::Magenta)
+        collider_change(position.x+105, position.y+100, 50, 20)
 {
     image.loadFromFile("images\\farmhouse.png");
     sprite.setTexture(image);
@@ -22,14 +16,6 @@ farmhouse::farmhouse(sf::Vector2f position):
 
 void farmhouse::draw(sf::RenderWindow &window){
     window.draw(sprite);
-    upper_rectangle.draw(window);
-
-    left_rectangle.draw(window);
-    right_rectangle.draw(window);
-
-    lower_left_rectangle.draw(window);
-    lower_right_rectangle.draw(window);
-    rectangle_change.draw(window);
 }
 
 std::vector<sf::FloatRect> farmhouse::getColliders() {
