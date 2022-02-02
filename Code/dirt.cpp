5a938da6cae4d054c8dd5a98ddd6c4a04784e854
KyrillW <75068697+KyrillW@users.noreply.github.com>
2022-02-02 16:31:55 +0100

    Update dirt.cpp

--- Code/dirt.cpp
+++ Code/dirt.cpp
@@ -128,3 +128,11 @@ bool dirt::getOwned() {
         return false;
     }
 } 
+
+void dirt::setPosition(sf::Vector2f newPos){
+    position = newPos;
+}
+
+void dirt::setState(state_t newState){
+    state = newState;
+}
