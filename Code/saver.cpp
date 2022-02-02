ae6cffb905c2c38a127e8f6a2526a1d815be9d93
KyrillW <75068697+KyrillW@users.noreply.github.com>
2022-02-02 16:50:27 +0100

    Update saver.cpp

--- Code/saver.cpp
+++ Code/saver.cpp
@@ -2,23 +2,27 @@
 #include <fstream>
 #include <sstream>
 
-saver::saver(player &Player, std::vector<std::vector<dirt*>> & farmlands, inventory * inv):Player(Player), farmlands(farmlands), inv(inv){}
+saver::saver(player &Player, std::vector<std::vector<dirt *>> &farmlands, inventory *inv, marketplace *Market) : Player(Player), farmlands(farmlands), inv(inv), Market(Market) {}
 
-
-void saver::save(std::string file){
+void saver::save(std::string file)
+{
     std::ofstream save_file(file);
     std::stringstream buffer;
+    buffer << *Market;
     buffer << Player;
     buffer << "!";
-    for(auto farmland : farmlands){
-        for(auto Dirt : farmland) {
+    for (auto farmland : farmlands)
+    {
+        for (auto Dirt : farmland)
+        {
             buffer << *Dirt;
             buffer << "!";
         }
     }
-    buffer << * inv;
+    buffer << "#";
+    buffer << *inv;
     std::string temp = buffer.str();
-    std::string encoded = base64_encode((unsigned char * const)temp.c_str(),temp.size());
+    std::string encoded = base64_encode((unsigned char *const)temp.c_str(), temp.size());
     save_file << encoded;
     save_file.close();
 }
