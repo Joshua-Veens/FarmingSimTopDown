0dd2012b044beebb89a90dc43fc8ab9e711044ae
KyrillW <75068697+KyrillW@users.noreply.github.com>
2022-02-02 16:41:21 +0100

    Update loader.cpp

--- Code/loader.cpp
+++ Code/loader.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 loader::loader() {}
 
-player *loader::loadPlayer(player *P)
+player *loader::loadPlayerAndMoney(player *P, marketplace *market)
 {
     std::ifstream rFile("save.txt");
     std::ostringstream buffer;
@@ -42,10 +42,11 @@ player *loader::loadPlayer(player *P)
         active = combine;
     }
     // std::cout << active;
-    std::string money = player_str.substr(player_str.find(": ") + 2, player_str.find(" Act") - 7);
+    std::string money = player_str.substr(player_str.find("Money: ") + 7, player_str.find(" Act") - 5);
+    // std::cout << player_str << std::endl;
+    // std::cout << money;
+    //
     int mon = std::stoi(money);
-    int mon1 = mon;
-    mon = mon1;
     std::string active_pos = activeVehicle.substr(activeVehicle.find("@") + 1, activeVehicle.size());
     std::string x_str = active_pos.substr(0, active_pos.find(","));
     std::string y_str = active_pos.substr(active_pos.find(",") + 1, active_pos.size());
@@ -73,8 +74,99 @@ player *loader::loadPlayer(player *P)
     {
         P->getVehicles()[trekker]->setPosition(location_inactive);
     }
-    P->setMoney(mon);
+    market->setMoney(mon);
     P->setVehicle(active);
     rFile.close();
     return P;
+}
+
+bool str_contains(std::string str, char cmp)
+{
+    for (auto n : str)
+    {
+        if (n == cmp)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+std::vector<std::vector<dirt *>> loader::loadFarms(std::vector<std::vector<dirt *>> farms)
+{
+    std::ifstream rFile("save.txt");
+    std::ostringstream buffer;
+    std::string temp;
+    rFile >> temp;
+    if (rFile.tellg() == 0)
+    {
+        rFile.close();
+        return farms;
+    }
+    // char ch;
+    // while(rFile.get(ch)){
+    //     buffer << ch;
+    //     std::cout << "load";
+    // }
+    // std::string to_decode = buffer.str();
+    std::string decoded = base64_decode(temp);
+    if (decoded.size() <= 0)
+    {
+        return farms;
+    }
+    std::string farm_strs = decoded.substr(decoded.find('!') - 1, decoded.size());
+    // std::cout << farm_strs;
+    std::string next_term = "";
+    std::vector<std::string> strings;
+    while (!str_contains(next_term, '#'))
+    {
+        // std::cout << next_term << std::endl;
+        strings.push_back(next_term);
+        farm_strs.erase(farm_strs.find('!'), farm_strs.find('!'));
+        next_term = farm_strs.substr(farm_strs.find('!'), farm_strs.find('!') + 1);
+    }
+    int i = 0;
+    // strings.pop_back();
+    std::vector<std::vector<int>> parsed;
+    for (auto str : strings)
+    {
+        parsed.push_back({});
+        str.erase(0, 6);
+        str.erase(0, str.find_first_not_of('0'));
+        std::string x_str = str.substr(0, str.find_first_of(','));
+        int x = std::atoi(x_str.c_str());
+        parsed[i].push_back(x);
+        str.erase(0, str.find_first_of(',') + 1);
+        str.erase(0, str.find_first_not_of('0'));
+        std::string y_str = str.substr(0, str.find_first_of(' '));
+        int y = std::atoi(y_str.c_str());
+        parsed[i].push_back(y);
+        str.erase(0, str.find_first_of('=') + 1);
+        int state = atoi(str.substr(0, str.find_first_of(' ')).c_str());
+        parsed[i].push_back(state);
+        str.erase(0, str.find_first_of('=') + 1);
+        int crop = atoi(str.substr(0, str.find_first_of('c')).c_str());
+        parsed[i].push_back(crop);
+        str.erase(0, str.find_first_of('=') + 1);
+        int owned = atoi(str.substr(0, str.find_first_of('!')).c_str());
+        parsed[i].push_back(owned);
+        str.erase(0, str.find_first_of('!') + 1);
+        i++;
+    }
+    int j = 0;
+    for(auto x : farms){
+        for(auto y : x){
+            y->setPosition(sf::Vector2f(parsed[j][0],parsed[j][1]));
+            y->setState((dirt::state_t)parsed[j][2]);
+            if(parsed[j][3] == corn){
+                y->changeToCorn();
+            }else{
+                y->changeToWheat();
+            }
+            if(parsed[j][4] == dirt::owned){
+                y->setToOwned();
+            }
+            j++;
+        }
+    }
+    return farms;
 }
