86b18791c7aa9bdb7a1138d9d678331c8a3521e4
ErikSchipper-HU <75141135+ErikSchipper-HU@users.noreply.github.com>
2022-02-04 20:18:27 +0100

    switch menu doxy

--- Code/switchMenu.hpp
+++ Code/switchMenu.hpp
@@ -1,14 +1,21 @@
+// this file contains doxygen lines
 #ifndef SWITCHMENU_HPP
 #define SWITCHMENU_HPP
 #include "menu.hpp"
 #include "player.hpp"
 
+/// @file switchMenu.hpp
+/// @brief switchMenu class
+/// @details the menu class to switch between vehicles. it uses the superclass menu
+
 class switchMenu : public menu{
 private:
     player * Player;
 public:
+    /// @brief constructor of the switch menu
     switchMenu(sf::RenderWindow & window, saver save, player * Player);
 
+    /// @brief creates the menu on the screen
     void create() override;
 };
 #endif
