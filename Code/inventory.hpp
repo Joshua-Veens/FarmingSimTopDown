a860efde4faef7bbf3df8aba09bccdfa778615fd
ErikSchipper-HU <75141135+ErikSchipper-HU@users.noreply.github.com>
2022-02-04 19:43:19 +0100

    inventory doxy

--- Code/inventory.hpp
+++ Code/inventory.hpp
@@ -32,63 +32,82 @@ private:
     sf::Texture wheatImage;
     sf::Texture cornImage;
     sf::Texture weedImage;
+
+    void drawWheat(sf::RenderWindow &window);
+    void drawCorn(sf::RenderWindow &window);
+    void drawWeed(sf::RenderWindow &window);
 public:
+    /// @brief constructor of inventory
     inventory(sf::Vector2f position);
 
+    /// @brief draws an inventory
     void draw( sf::RenderWindow & window ) override;
 
+    /// @brief draws the silo on the screen
     void drawSilo(sf::RenderWindow & window);
 
+    /// @brief send wheat amount
     int getWheat();
 
+    /// @brief send corn amount
     int getCorn();
 
+    /// @brief send weed amount
     int getWeed();
 
-
+    /// @brief set a wheat amount
     void setWheat(int newWheat);
 
+    /// @brief set a corn amount
     void setCorn(int newCorn);
 
+    /// @brief set a weed amount
     void setWeed(int newWeed);
 
+    /// @brief set all crops to 0
     void removeCrops();
     
     friend std::ostream &operator<<(std::ostream &lhs, inventory & inv)
     {
         return lhs << "Wheat: " << inv.wheatCount << " Corn: "<< inv.cornCount << "weed: " << inv.weedCount;
     }
-
+    /// @brief adds an amount to wheat
     void addWheat(unsigned int wheat);
 
+    /// @brief adds an amount to corn
     void addCorn(unsigned int corn);
 
+    /// @brief adds an amount to weed
     void addWeed(unsigned int weed);
 
+    /// @brief removes an amount of wheat
     void removeWheat(unsigned int wheat);
 
+    /// @brief removes an amount of corn
     void removeCorn(unsigned int corn);
 
+    /// @brief removes an amount of weed
     void removeWeed(unsigned int weed);
 
+    /// @brief draws the icons for the crops
     void drawInventory(sf::RenderWindow & window);
 
-    void drawWheat(sf::RenderWindow &window);
-
-    void drawCorn(sf::RenderWindow &window);
-
-    void drawWeed(sf::RenderWindow &window);
-
+    /// @brief sends box of the silo colliders
     sf::FloatRect getCollider() override;
 
+    /// @brief sends the deposit collider box
     sf::FloatRect getDepositCollider();
 
+    /// @brief sends current position
     sf::Vector2f getPosition();
 
+    /// @brief sends box for wheat button
     sf::FloatRect getWheatTextCollider();
 
+    /// @brief sends box for corn button
     sf::FloatRect getCornTextCollider();
 
+    /// @brief sends box for weed button
     sf::FloatRect getWeedTextCollider();
 };
 
