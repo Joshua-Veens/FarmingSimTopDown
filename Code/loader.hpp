9bf3654e04c6d33879024e3a0561181957bb9ac1
justinwiel <70429645+justinwiel@users.noreply.github.com>
2022-02-04 15:28:46 +0100

    added doxygen to saver and loader

--- Code/loader.hpp
+++ Code/loader.hpp
@@ -8,12 +8,25 @@
 #include "marketplace.hpp"
 #include "tractor.hpp"
 #include "vehicle_shop.hpp"
+///@file contains the loader class
+///@details loads the entire game  from a save file
 class loader{
 private: 
 public:
+    ///@brief loader constructor, doesn't take any parameters
     loader();
+    ///@brief loads player and money into player and market
+    ///@param P pointer to player
+    ///@param market pointer to market
     static player * loadPlayerAndMoney(player * P, marketplace * market);
-    static std::vector<std::vector<dirt *>>   loadFarms(std::vector<std::vector<dirt *>> );
+    ///@brief loads all the farmlands
+    ///@param farms vector of vectors containing dirt
+    static std::vector<std::vector<dirt *>>   loadFarms(std::vector<std::vector<dirt *>> farms );
+    ///@brief loads the silo and the vehicles
+    ///@param silo pointer to inventory
+    ///@param Trekker pointer to tractor
+    ///@param Combine pointer to harvester
+    ///@param VS pointer to vehicleShop
     inventory * loadSiloAndVehicles(inventory * silo, tractor * Trekker, harvester * Combine, vehicle_shop * VS);
 
 };
--- Code/saver.hpp
+++ Code/saver.hpp
@@ -8,6 +8,9 @@
 #include "marketplace.hpp"
 #include "vehicle_shop.hpp"
 #include <string>
+
+///@file this file contains the class that causes the entire game to be saved
+///@details saver class saves all the things that need saving
 class saver {
 private: 
     player & Player;
@@ -18,8 +21,18 @@ private:
     tractor * trekker;
     vehicle_shop * VS;
 public:
+    ///@brief saver class constructor
+    ///@param Player reference to player class
+    ///@param Farmlands reference to vector farmlands, which is vecotor<vector<int>>
+    ///@param inv pointer to inventory
+    ///@param Market pointer to marketplace
+    ///@param combine  pointer to harvester
+    ///@param trekker pointer to tractor
+    ///@param VS pointer to vehicleShop
     saver(player & Player, std::vector<std::vector<dirt *>> & farmlands, inventory * inv, marketplace * Market, harvester * combine, tractor * trekker,  vehicle_shop * VS);
 
+    ///@brief saves the game
+    ///@param file desired save file name
     void save(std::string file);
 
 };
