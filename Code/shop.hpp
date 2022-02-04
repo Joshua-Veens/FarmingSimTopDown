dbb7a2980beb63885a1c5a66b35685bd02d5aac6
ErikSchipper-HU <75141135+ErikSchipper-HU@users.noreply.github.com>
2022-02-04 20:24:42 +0100

    shop doxy

--- Code/shop.hpp
+++ Code/shop.hpp
@@ -1,8 +1,13 @@
+// this file contains doxygen files
 #ifndef V2CPSE2_EXAMPLES_SHOP_HPP
 #define V2CPSE2_EXAMPLES_SHOP_HPP
 
 #include "dirt.hpp"
 #include "marketplace.hpp"
+
+/// @file shop.hpp
+/// @brief shop class
+/// @details makes the shop class. It makes it possible to buy farmland.
 
 class shop : public drawable{
 private:
@@ -26,12 +31,18 @@ private:
     void drawNotEnoughMoney(sf::RenderWindow &window);
 
 public:
+    /// @brief constructor of shop class
     shop(std::vector<std::vector<dirt *>> & farmlands, marketplace * market, sf::Clock & clock);
 
+    /// @brief makes the pillars next to farmlands witch a player is able to buy
     void draw(sf::RenderWindow &window);
 
+    /// @brief draws the pillars
     void addForSaleSign(sf::Vector2f position);
 
+    /// @brief changes the land if bought by a player.
+    /// @details first checks if player has enough money to buy the piece of land later removes the amount for the land from the players money
+    /// later removes the for sale sign pillars
     void buyLand(sf::Vector2f mouse, sf::RenderWindow &window);
 };
 
