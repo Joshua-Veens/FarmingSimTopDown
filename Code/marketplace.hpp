4b5b846248e58bd14b6e61b93945f371a00eb041
ErikSchipper-HU <75141135+ErikSchipper-HU@users.noreply.github.com>
2022-02-04 20:02:38 +0100

    marketplace doxy

--- Code/marketplace.hpp
+++ Code/marketplace.hpp
@@ -1,7 +1,14 @@
+// this file contains doxygen lines
+
 #ifndef V2CPSE2_EXAMPLES_MARKETPLACE_HPP
 #define V2CPSE2_EXAMPLES_MARKETPLACE_HPP
 
+
 #include "drawable.hpp"
+
+/// @file marketplace.hpp
+/// @brief marketplace class
+/// @details this class keeps track of the players amount of money
 
 class marketplace : public drawable{
 private:
@@ -16,26 +23,38 @@ private:
     sf::Font font;
     sf::Text text;
 public:
+
+    /// @brief constructor of the marketplace
     marketplace(sf::Vector2f position);
 
+    /// @brief draws the marketplace on the screen
     void draw(sf::RenderWindow & window) override;
 
+    /// @brief sends collisition box of the marketplace
     sf::FloatRect getCollider() override;
 
+    /// @brief sends collisition box of the sell part of the marketplace
     sf::FloatRect getSellCollider();
 
+    /// @brief sends current position
     sf::Vector2f getPosition();
 
+    /// @brief changes money amount of a player
     void sellCrops(float wheat, float corn, float weed);
 
+    /// @brief draws the money amount of a player
     void drawMoney(sf::RenderWindow & window, sf::Vector2f moneyPosition);
-    
+
+    /// @brief sends the money amunt of a player
     float getMoney();
-    
+
+    /// @brief adds an amount of money to the currect money amount
     void addMoney(float moneyToAdd);
-    
+
+    /// @brief sets a money amount
     void setMoney(float allMoney);
-    
+
+    /// @brief removes an amount of money from the current money amount
     void removeMoney(float moneyToRemove);
     
     friend std::ostream & operator<<(std::ostream & lhs, marketplace & Market){
